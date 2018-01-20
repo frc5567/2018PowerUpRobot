e8d04bda214968f7a668c66d94227c27c8b1111b
Josh <josh.overbeek@outlook.com>
2018-01-20 13:48:31 -0500

    Added pixy vision classes

---
+++ src/org/usfirst/frc/team5567/robot/PixyPacket.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team5567.robot;
+
+
+
+public class PixyPacket {
+
+	double x, y;
+
+	double area;
+
+	
+
+	//for our use we only cared about a x, y, and area
+
+	//you could get any data you wanted
+
+}
